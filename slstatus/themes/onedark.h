1a6f448ac0a279e3f802e4fc8dcc1aae68345b42
Momin Rather <momin.rather@gmail.com>
2022-09-09 18:01:44 +0530

    suckless

---
+++ slstatus/themes/onedark.h
@@ -0,0 +1,9 @@
+static const struct arg args[] = {
+	{ cpu_perc,			"^c#56b6c2^ ﬙  %s% | ",			  NULL },
+	{ ram_perc,			"^c#c678dd^   %s% | ",			  NULL },
+	{ wifi_essid,		"^c#61afef^   %s ",		    "wlo1" },
+	{ netspeed_rx,		"^c#61afef^   %s  | ",		    "wlo1" },
+	{ vol_perc,			"^c#e5c07b^ 墳 %s%% | ", "/dev/mixer"  },
+	{ battery_perc,		"^c#98c379^  %s%% | ",         "BAT1" },
+	{ datetime,			"^c#be5046^   %s ",           "%F %r" },
+};
